8d5caa9da184cf72e3bbbdc7d6ab752d9697df18
Mettle X <56449790+mettlex@users.noreply.github.com>
2022-01-17 21:27:15 +0600

    fix cors

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -29,7 +29,7 @@ export const handleRequest = async (request: Request): Promise<Response> => {
   }
 
   // Set CORS headers
-  response.headers.set("Access-Control-Allow-Origin", url.origin);
+  response.headers.set("Access-Control-Allow-Origin", "*");
 
   // Append to/Add Vary header so browser will cache response correctly
   response.headers.append("Vary", "Origin");
