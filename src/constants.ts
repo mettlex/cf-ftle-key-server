d8fe751638a70e859373edb863f77799abe2bab6
Mettle X <56449790+mettlex@users.noreply.github.com>
2022-01-17 21:27:07 +0600

    seperate constant vars

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const key_derivation_iterations = 100_000;
+export const key_derivation_algorithom = "PBKDF2";
+export const hash_algorithm = "SHA-512";
+export const encryption_algorithm = "AES-GCM";
